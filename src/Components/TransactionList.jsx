c4d273b57aa1a8bb58bfe86c734d50b5c46c12b6
Evelyn Chen <evelync93@gmail.com>
2019-10-09 16:54:24 -0400

    update style and add graph componentn

--- src/Components/TransactionList.jsx
+++ src/Components/TransactionList.jsx
@@ -5,7 +5,7 @@ import TransactionListEntry from "./TransactionListEntry";
 export default function TransactionList() {
   console.log("data", data);
   return (
-    <div>
+    <div className="txn">
       <h3>Transactions</h3>
       <div className="txn-table">
         <div className="txn-header txn-row">
@@ -13,6 +13,8 @@ export default function TransactionList() {
           <div className="txn-data">Description</div>
           <div className="txn-data">Amount</div>
           <div className="txn-data">Category</div>
+        </div>
+        <div className=" txn-rows">
           {data.map(txn => {
             return <TransactionListEntry txn={txn} />;
           })}
